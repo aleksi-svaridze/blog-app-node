217f0b60255ec2fce6b24bd45556a19512416a74
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-03-25 13:56:22 +0400

    Create database and connect to the server

--- config/db.js
+++ config/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.DB_URI);
+        console.log('success');
     } catch (error) {
-        
+        console.log(`Error - ${error}`);
+        process.exit(1);
     }
 }
 
