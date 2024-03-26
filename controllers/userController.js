e3d4c456e31795e8bf5b92be1609df17d91ad632
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-03-26 09:51:52 +0400

    Add custom error handler in middleware folder

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,14 @@
 import User from '../models/User'
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
     try {
         const {name, email, password} = req.body;
 
         let user = await User.findOne({email});
 
         if(user) {
-            return res.status(400).json({message: "User have already registered"})
+            // return res.status(400).json({message: "User have already registered"})
+            throw new Error("User have already registered")
         }
 
         user = await User.create({
@@ -27,7 +28,8 @@ export const registerUser = async (req, res) => {
         })
 
     } catch (error) {
-        return res.status(500).json({message: `Something went wrong - ${error}`})
+        // return res.status(500).json({message: `Something went wrong - ${error}`});
+        next(error);
     }
 }
 
