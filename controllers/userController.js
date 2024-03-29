7535ae734588d120ebef6a5c86d8be73bafccdda
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-03-29 14:23:41 +0400

    Add updateProfile controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,4 +83,36 @@ export const userProfile = async (req, res, next) => {
     }
 }
 
-export {registerUser, loginUser}
+export const updateProfile = async (req, res, next) => {
+    try {
+        let user = await User.findById(req.user._id);
+
+        if(!user){
+            throw new Error('User not found!')
+        }
+
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password && req.body.password.length < 6) {
+            throw new Error('Password must be at least 6 character');
+        } else if (req.body.password){
+            user.password = req.body.password;
+        }
+
+        const updatedUserProfile = await user.save();
+
+        res.json({
+            _id: updatedUserProfile._id,
+            avatar: updatedUserProfile.avatar,
+            name: updatedUserProfile.name,
+            email: updatedUserProfile.email,
+            verified: updatedUserProfile.verified,
+            admin: updatedUserProfile.admin,
+            token: await updatedUserProfile.generateJWT(),
+        })
+        
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { registerUser, loginUser, userProfile } from '../controllers/userController';
+import { registerUser, loginUser, userProfile, updateProfile } from '../controllers/userController';
 import { authGuard } from '../middleware/authMiddleware';
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
+router.put("/updateProfile", authGuard, updateProfile);
 
 export default router;
